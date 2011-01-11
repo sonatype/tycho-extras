5c47fff088b1b7abceb29d24c98b0bbb9f122798
Tobias Oberlies <tobias.oberlies@sap.com>
2011-01-11 15:22:00 +0100

    Added features and bundles publisher in new tycho-p2-extras-plugin
    - PublishFeatureAndBundlesMojo implementation wraps
      org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher
    - README.txt explains the usage of the features and bundles publisher by means
      of the testProject in tycho-p2-extras-plugin/src/test/resources
    - Added junit test

---
+++ tycho-p2-extras-plugin/src/test/java/org/sonatype/tycho/plugins/p2/extras/PublishFeaturesAndBundlesMojoTest.java
@@ -0,0 +1,138 @@
+package org.sonatype.tycho.plugins.p2.extras;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+import junit.framework.Assert;
+
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.IOUtil;
+import org.apache.maven.plugin.LegacySupport;
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.repository.RepositorySystem;
+import org.codehaus.tycho.testing.AbstractTychoMojoTestCase;
+
+import de.pdark.decentxml.Document;
+import de.pdark.decentxml.Element;
+import de.pdark.decentxml.XMLIOSource;
+import de.pdark.decentxml.XMLParser;
+
+public class PublishFeaturesAndBundlesMojoTest
+    extends AbstractTychoMojoTestCase
+{
+
+    public void testPublisher()
+        throws Exception
+    {
+        File basedir = getBasedir( "testProject" );
+        List<MavenProject> projects = getSortedProjects( basedir, null );
+        MavenProject project = projects.get( 0 );
+
+        initLegacySupport( projects, project );
+
+        // simulate that content to be published has already been extracted to the target folder
+        File sourceRepositoryDir =
+            new File( project.getFile().getParent(), "target/sourceRepository" ).getAbsoluteFile();
+        generateContentToBePublished( sourceRepositoryDir );
+
+        File publishedContentDir = new File( project.getFile().getParent(), "target/repository" ).getAbsoluteFile();
+
+        // call publisher mojo
+        Mojo publishMojo = lookupMojo( "publish-features-and-bundles", project.getFile() );
+        setVariableValueToObject( publishMojo, "project", project );
+        setVariableValueToObject( publishMojo, "sourceLocation", sourceRepositoryDir.toString() );
+        setVariableValueToObject( publishMojo, "artifactRepositoryLocation", publishedContentDir.toString() );
+        setVariableValueToObject( publishMojo, "metadataRepositoryLocation", publishedContentDir.toString() );
+        setVariableValueToObject( publishMojo, "publishArtifacts", Boolean.TRUE );
+
+        publishMojo.execute();
+
+        assertPublishedIU( publishedContentDir, "org.sonatype.tycho.extras.testdata" );
+        assertPublishedArtifact( publishedContentDir, "org.sonatype.tycho.extras.testdata", "1.0.0" );
+    }
+
+    private void assertPublishedArtifact( File publishedContentDir, String bundleName, String version )
+    {
+        String artifactFileName = bundleName + "_" + version; // without qualifier
+        File publishedArtifact = ( new File( publishedContentDir, "plugins" ).listFiles() )[0];
+
+        Assert.assertTrue( "Published artifact not found: " + artifactFileName,
+                           publishedArtifact.getName().startsWith( artifactFileName ) );
+
+    }
+
+    private static void assertPublishedIU( File publishedContentDir, String iuID )
+        throws IOException
+    {
+        XMLParser parser = new XMLParser();
+        Document document = parser.parse( new XMLIOSource( new File( publishedContentDir, "content.xml" ) ) );
+        Element unitElement = document.getChild( "repository/units" );
+        List<Element> children = unitElement.getChildren( "unit" );
+        for ( Element element : children )
+        {
+            if ( iuID.equals( element.getAttribute( "id" ).getValue() ) )
+            {
+                return;
+            }
+        }
+        Assert.fail( "IU not found: " + iuID );
+    }
+
+    private void initLegacySupport( List<MavenProject> projects, MavenProject currentProject )
+        throws Exception
+    {
+        MavenSession session = newMavenSession( currentProject, projects );
+        System.out.println( session.getLocalRepository() );
+        LegacySupport buildContext = lookup( LegacySupport.class );
+        buildContext.setSession( session );
+    }
+
+    private void generateContentToBePublished( File repositoryFolder )
+        throws IOException
+    {
+        String bundleFileName = "testdata-1.0.0-SNAPSHOT.jar";
+        URL source = getClassLoader().getResource( bundleFileName );
+        FileUtils.copyURLToFile( source, new File( repositoryFolder, "plugins/" + bundleFileName ) );
+    }
+
+    // use the normal local Maven repository (called by newMavenSession)
+    @Override
+    protected ArtifactRepository getLocalRepository()
+        throws Exception
+    {
+        RepositorySystem repoSystem = lookup( RepositorySystem.class );
+        File path = getLocalMavenRepository().getCanonicalFile();
+        ArtifactRepository r = repoSystem.createLocalRepository( path );
+        return r;
+    }
+
+    private File getLocalMavenRepository()
+    {
+        /*
+         * The build (more specifically, the maven-properties-plugin) writes the local Maven repository location to a
+         * file. Here, we read this file. (Approach copied from tycho-its.)
+         */
+        Properties buildProperties = new Properties();
+        InputStream is = this.getClassLoader().getResourceAsStream( "baseTest.properties" );
+        try
+        {
+            buildProperties.load( is );
+        }
+        catch ( IOException e )
+        {
+            throw new RuntimeException( e );
+        }
+        finally
+        {
+            IOUtil.close( is );
+        }
+        return new File( buildProperties.getProperty( "local-repo" ) );
+    }
+}
